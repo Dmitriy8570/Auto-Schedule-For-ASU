3b9b4d0b96eb6ecfea8614c0bf2d43277b0ce664
dmitriy8570 <bardakov.dmitriy09@gmail.com>
2026-02-22 13:24:47 +0700

    fix-authentication

--- backend-auto-schedule/API/Controllers/AuthenticationController.cs
+++ backend-auto-schedule/API/Controllers/AuthenticationController.cs
@@ -5,18 +5,28 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    public class LoginRequest
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+    }
     public class AuthenticationController : ControllerBase
     {
-        private string Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        private LoginResponse response = new LoginResponse { Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"};
         private string Login = "admin";
         private string Password = "password";
 
         [HttpPost]
-        public IActionResult Post(string login, string password)
+        public IActionResult Post([FromBody] LoginRequest request)
         {
-            if (login == Login && password == Password)
+            if (request.Login == Login && request.Password == Password)
             {
-                return Ok(Key);
+                return Ok(response);
             }
             return Unauthorized();
         }
