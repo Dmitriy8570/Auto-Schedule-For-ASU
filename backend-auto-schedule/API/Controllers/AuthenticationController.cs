e830ae074fff178d4c69da21b84ae4b9ba9be555
dmitriy8570 <bardakov.dmitriy09@gmail.com>
2026-02-22 12:45:14 +0700

    add-authentication

---
+++ backend-auto-schedule/API/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private string Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        private string Login = "admin";
+        private string Password = "password";
+
+        [HttpPost]
+        public IActionResult Post(string login, string password)
+        {
+            if (login == Login && password == Password)
+            {
+                return Ok(Key);
+            }
+            return Unauthorized();
+        }
+    }
+}
