7480a368ce4f2224bac1ce1c9f96028dbf608e33
dmitriy8570 <bardakov.dmitriy09@gmail.com>
2026-02-24 08:23:08 +0700

    start-write-static-json-response

---
+++ backend-auto-schedule/API/entites/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace API.entites
+{
+    public class Department
+    {
+        int id { get; set; }
+        string name { get; set; }
+        Institute institute { get; set; }
+    }
+}
---
+++ backend-auto-schedule/API/entites/Lesson.cs
@@ -0,0 +1,9 @@
+﻿namespace API.entites
+{
+    public class Lesson
+    {
+        int id { get; set; }
+        Classroom classroom { get; set; }
+
+    }
+}
