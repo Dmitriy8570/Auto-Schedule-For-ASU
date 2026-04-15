d30b332e1160178ff4072de38d7745bcc661a176
KsuhaBer <kseniaberlova2004@gmail.com>
2026-04-16 00:12:11 +0700

    files

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,1 +1,12 @@
-<template>"Привет, я здесь!"</template>
+<template>
+  <div class="app-layout">
+    <h1 style="color: white">Проверка связи!</h1>
+  </div>
+</template>
+
+<style scoped>
+.app-layout {
+  min-height: 100vh; /* Говорим: растянись на 100% высоты экрана */
+  background-color: #1a56db; /* Красим в синий */
+}
+</style>
