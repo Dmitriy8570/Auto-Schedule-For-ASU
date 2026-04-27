68e16a50702172ec2613da585eb8509169da2c42
dmitriy8570 <bardakov.dmitriy09@gmail.com>
2026-04-27 21:16:40 +0700

    add Querys(by Room, Group, Teacher) for lesson

---
+++ backend-auto-schedule/src/Application/Common/Lessons/Commands/CreateLessonCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Common.Lessons.Commands;
+
+public class CreateLessonCommandValidator : AbstractValidator<CreateLessonCommand>
+{
+    public CreateLessonCommandValidator()
+    {
+        RuleFor(x => x.ClassroomId).NotEmpty().WithMessage("ClassroomId is required.");
+        RuleFor(x => x.TimeSlotId).NotEmpty().WithMessage("TimeSlotId is required.");
+        RuleFor(x => x.StreamId).NotEmpty().WithMessage("StreamId is required.");
+    }
+}
---
+++ backend-auto-schedule/src/Application/Common/Lessons/Querys/GetLessonByGroupQuery.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.DTO.Lessons;
+using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.Common.Lessons.Querys;
+
+public class GetLessonByGroupQuery: IRequest<IReadOnlyList<LessonDTO>>
+{
+    public Guid GroupId { get; init; }
+}
+
+public class GetLessonByGroupQueryHandler : IRequestHandler<GetLessonByGroupQuery, IReadOnlyList<LessonDTO>>
+{
+    private readonly ILessonRepository _lessonRepository;
+    public GetLessonByGroupQueryHandler(ILessonRepository lessonRepository)
+    {
+        _lessonRepository = lessonRepository;
+    }
+    public async Task<IReadOnlyList<LessonDTO>> Handle(GetLessonByGroupQuery request, CancellationToken cancellationToken)
+    {
+        var lessons = await _lessonRepository.GetLessonByGroupAsync(request.GroupId, cancellationToken);
+        return lessons?.Select(lesson => new LessonDTO
+        {
+            Id = lesson.Id,
+            ClassroomId = lesson.ClassroomId,
+            TimeSlotId = lesson.TimeSlotId,
+            StreamId = lesson.StreamId
+        }).ToList() ?? new List<LessonDTO>();
+    }
+}
