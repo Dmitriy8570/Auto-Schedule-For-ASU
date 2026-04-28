6d75fab2b4825444d1342c158e26566bbeb7e320
dmitriy8570 <bardakov.dmitriy09@gmail.com>
2026-04-28 08:14:10 +0700

    create Infrastructure layer

--- backend-auto-schedule/src/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ backend-auto-schedule/src/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68e16a50702172ec2613da585eb8509169da2c42")]
-[assembly: System.Reflection.AssemblyProductAttribute("Domain")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
+[assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
